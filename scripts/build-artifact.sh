1c2f37e87e229060e334b378fa40ddcf93f7c590
Rumato Estorsky <rumatoest@users.noreply.github.com>
2022-09-07 13:48:32 +0300

    Skip checks for test tags

--- scripts/build-artifact.sh
+++ scripts/build-artifact.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
-cargo update
 cargo build --target wasm32-unknown-unknown --release
 ic-cdk-optimizer target/wasm32-unknown-unknown/release/hello_ic.wasm -o .artifact/hello_ic.wasm
 cp ./src/hello_ic/hello_ic.did .artifact/
